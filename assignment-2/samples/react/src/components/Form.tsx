7263949aa50ce0d85ce6aa921e822e00d712ecd5
takehiro <takehiro-r133@hotmail.co.jp>
2022-05-31 19:05:42 +0900

    feat: InputTextコンポーネント追加 #3

--- assignment-2/samples/react/src/components/Form.tsx
+++ assignment-2/samples/react/src/components/Form.tsx
@@ -1,31 +1,71 @@
+import { useState } from "react"
 import styles from "./Form.module.scss"
+import { InputText } from "./InputText"
 
 export const Form: React.FC = () => {
+  /** 各入力欄のstate */
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [zip, setZip] = useState("")
+  const [prefecture, setPrefecture] = useState("")
+  const [address1, setAddress1] = useState("")
+  const [address2, setAddress2] = useState("")
+
+  /** バリデーションメッセージのstate */
+  const [nameValidation, setNameValidation] = useState("")
+  const [emailValidation, setEmailValidation] = useState("")
+  const [zipValidation, setZipValidation] = useState("")
+  const [prefectureValidation, setPrefectureValidation] = useState("")
+  const [address1Validation, setAddress1Validation] = useState("")
+
   return (
     <div className={styles.formContainer}>
       <div className={styles.inputAreaContainer}>
         <label>氏名</label>
-        <input type='text' />
+        <InputText
+          keyword={name}
+          setKeyword={setName}
+          validation={nameValidation}
+          placeholder='(例)トレタ太郎'
+        />
       </div>
       <div className={styles.inputAreaContainer}>
         <label>Eメール</label>
-        <input type='text' />
+        <InputText
+          keyword={email}
+          setKeyword={setEmail}
+          validation={emailValidation}
+          placeholder='(例)yoyaku@toreta.in'
+        />
       </div>
       <div className={styles.inputAreaContainer}>
         <label>郵便番号</label>
-        <input type='text' />
+        <InputText
+          keyword={zip}
+          setKeyword={setZip}
+          validation={zipValidation}
+          placeholder='(例)0000000'
+        />
       </div>
       <div className={styles.inputAreaContainer}>
         <label>都道府県</label>
-        <input type='text' />
       </div>
       <div className={styles.inputAreaContainer}>
         <label>市区町村・番地</label>
-        <input type='text' />
+        <InputText
+          keyword={address1}
+          setKeyword={setAddress1}
+          validation={address1Validation}
+          placeholder='(例)品川区西五反田７丁目２２−１７'
+        />
       </div>
       <div className={styles.inputAreaContainer}>
         <label>建物名・号室</label>
-        <input type='text' />
+        <InputText
+          keyword={address2}
+          setKeyword={setAddress2}
+          placeholder='(例)TOCビル8F'
+        />
       </div>
       <div className={styles.buttonContainer}>
         <button>登録</button>
---
+++ assignment-2/samples/react/src/components/InputText.tsx
@@ -0,0 +1,25 @@
+import styles from "./InputText.module.scss"
+import { Dispatch, SetStateAction, useCallback } from "react"
+
+type Props = {
+  keyword: string
+  setKeyword: Dispatch<SetStateAction<string>>
+  validation?: string
+  placeholder: string
+}
+export const InputText: React.FC<Props> = (props) => {
+  const { keyword, setKeyword, validation, placeholder } = props
+  const handleChangeKeyword = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      const { value } = e.currentTarget
+      setKeyword(value)
+    },
+    [setKeyword]
+  )
+  return (
+    <div className={styles.inputTextContainer}>
+      <input type='text' value={keyword} onChange={handleChangeKeyword} placeholder={placeholder} />
+      <span>{validation}</span>
+    </div>
+  )
+}
